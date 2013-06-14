bc44b578fef902d99aea003f0060ee6799c4f62c
Jordan Skarbek <jaskarbek@gmail.com>
2013-06-14 10:28:27 -0700

    app functioining

--- lib/decrypter.rb
+++ lib/decrypter.rb
@@ -3,9 +3,11 @@ class Decrypter
 
   attr_accessor :message
 
-  CODES = { 'A' => 'Z', 'B' => 'Y', 'C' => 'X'}
+  CODES = { 'A' => 'Z', 'B' => 'Y', 'C' => 'X', 'D' => 'W', 'E' => 'V',
+            'F' => 'U', 'G' => 'T', 'H' => 'S', 'I' => 'R', 'J' => 'Q',
+            'K' => 'P', 'L' => 'O' }
 
-  def initialize(message)
+  def initialize(message='ABC')
     @message = message
   end
 
@@ -14,31 +16,37 @@ class Decrypter
   #  BE ABLE TO USE LOCAL VARIABLES
   #  BE ABLE TO CREATE AND CALL CLASS AND INSTANCE METHODS
   #  BE ABLE TO USE LOOP
+  #  MEMORIZE COMMON ITERATORS AND CONDITIONAL STATEMENTS
   def self.start
-    puts "STARTING DECRYPTER V1.22 \n ... \n ... \n"
+    puts "STARTING DECRYPTER V1.22 \n ... \n ... \n ... \n"
     puts "ENTER MESSAGE TO DECRYPT: "
     loop do
-      user_input = gets
-      if user_input.upcase == "EXIT"
+      user_input = gets.upcase.chomp
+      if user_input == "EXIT"
         break
       else
-        encrypted_message = Decrypter.new(user_input).chomp!
-        decrypted_message = encrypted_message.upcase.decrypt
-        return decrypted_message
+        encrypted_message = Decrypter.new(user_input)
+        decrypted_message = encrypted_message.decrypt
+        puts decrypted_message
       end
     end
   end
 
   #  MEMORIZE THE STEPS FOR CREATING ALGORITHMS
+  #  UNDERSTAND THE DIFFERENCE BETWEEN A HASH AND AN ARRAY
   #  BE ABLE TO USE EACH
   def decrypt
-    decrypted = ""
-    self.each_char do |char|
-      new_char = CODES[char].value
-      decrypted += new_char
+    @message.each_char do |char|
+      if CODES.has_key?(char)
+        new_char = CODES[char]
+        @message.gsub!(char, new_char)
+      else
+        @message = "INVALID INPUT"
+      end
     end
-    return decrypted
+    return @message
   end
+
 end
 
 
